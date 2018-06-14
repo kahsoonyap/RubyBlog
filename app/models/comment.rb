31d8720ef2fde91ac0c9f5493ffa21035b1b2dc1
Kah Soon Yap <kah@sweeten.com>
2018-06-14 13:06:39 -0400

    refactored

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ApplicationRecord
 
-  belongs_to :article
+  belongs_to :article, dependent: :destroy
+
+  validates :title,
+    presence: true,
+    length: {
+      minimum: 3
+    }
+
+  validates :body,
+    presence: true,
+    length: {
+      minimum: 3
+    }
 
 end
